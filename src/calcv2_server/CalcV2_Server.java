5f46cce3f2e545830c7a91c13292cc0f2df51fc3
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-01-26 18:58:04 +0100

    "Calculadora SERVER V2 "

---
+++ src/calcv2_server/CalcV2_Server.java
@@ -0,0 +1,23 @@
+package calcv2_server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CalcV2_Server {
+    
+    public static void main(String[] args) {
+        
+        ServerUI obj = new ServerUI();
+        obj.setVisible(true);
+        
+        
+        
+    }
+    
+}
